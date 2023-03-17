5a5f5435490f45e9fef00aef03d7e994ab2c075a
Crgkrm <92501302+Crgkrm@users.noreply.github.com>
2023-03-16 22:04:22 -0700

    responsive

---
+++ orders.js
@@ -0,0 +1,55 @@
+const Orders=[
+{
+    productName: 'Foldable Mini Drone',
+    productNumber:'85631',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+
+{
+    productName: 'LARAVENDER KF102 Drone',
+    productNumber:'36378',
+    paymentStatus:'Refunded',
+    shipping:'Decline'
+}
+,
+{
+    productName: 'Ruko F1  Pro Drone',
+    productNumber:'49347',
+    paymentStatus:'Due',
+    shipping:'Pending'
+}
+,
+{
+    productName: 'Drone With camera  Drone',
+    productNumber:'96996',
+    paymentStatus:'Paid',
+    shipping:'Deliverd'
+}
+,
+{
+    productName: 'GPS 4K Drone',
+    productNumber:'22821',
+    paymentStatus:'Paid',
+    shipping:'Delivered'
+}
+,
+{
+    productName: 'Lozenge Drone',
+    productNumber:'00482',
+    paymentStatus:'Paid',
+    shipping:'Delivered'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+]
